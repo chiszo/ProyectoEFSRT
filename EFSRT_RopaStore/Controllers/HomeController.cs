343368c8493c0045d94c8e7bff775e3701dc05df
chiszo <gigibethandrea@gmail.com>
2023-10-03 12:22:31 -0500

    creando repo y carpetas

---
+++ EFSRT_RopaStore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EFSRT_RopaStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EFSRT_RopaStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EFSRT_RopaStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFSRT_RopaStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
